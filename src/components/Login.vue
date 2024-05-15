eb36f369e2c1b0b2945692c0a249d6166123fe52
José R. Ferreyra <jferreyra.dev@gmail.com>
2024-05-15 16:02:25 -0300

    inicial

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      // Send login request and handle authentication token
+      // Store token in local storage
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input type="email" v-model="email" placeholder="Email" />
+    <input type="password" v-model="password" placeholder="Password" />
+    <button @click="login">Login</button>
+  </div>
+</template>
